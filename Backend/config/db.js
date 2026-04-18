065e4d6ec5f395dbb4cb543de12ae7f3957b817a
tejnath35 <tejnathg35@gmail.com>
2026-04-19 00:02:45 +0530

    s

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected successfully");
   } catch (error) {
-    console.error("MongoDB connection failed");
+    console.error("MongoDB connection failed:", error.message);
     process.exit(1);
   }
 };
